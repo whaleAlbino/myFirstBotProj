c08493be18d1264cd23e1a943aaadb3e75dddf42
whaleAlbino <100727797+whaleAlbino@users.noreply.github.com>
2022-05-20 00:53:20 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+
+TOKEN = '5107104138:AAHQ0ozSMQt_IHUmDd_Op9KP0xxkZ5ZdZmQ'
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+}
---
+++ myBot.py
@@ -0,0 +1,44 @@
+import telebot
+from config import keys, TOKEN
+from extensions import APIException, CurrencyConverter
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу с ботом, введите команду в следующем формате: \n<имя валюты> \
+<в какую валюту перевести> \
+<количество переводимой валюты>\nУвидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = "Доступные валюты:"
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise APIException('Неверное количество параметров')
+
+        quote, base, amount = values
+        f_am = float(amount)
+        total_base = CurrencyConverter.get_price(quote, base, amount) * f_am
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} = {round(total_base, 2)}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling(none_stop=True)
